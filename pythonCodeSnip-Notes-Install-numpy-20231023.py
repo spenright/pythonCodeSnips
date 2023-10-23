9206171b1535c433587f4d83b7cd1834c15cb546
spenright <stephen.p.enright@gmail.com>
2023-10-23 11:37:44 -0700

    Added A Notes File. The notes file shows my commandline to install the numpy module for its anciallary support of arrarys.

---
+++ pythonCodeSnip-Notes-Install-numpy-20231023.py
@@ -0,0 +1,25 @@
+""" 
+
+NOTES ONLY!
+
+My real Windows username was replaced with "{windows-username}" to 'protect the innocent'. ;-)
+
+
+PowerShell 7.3.8
+PS C:\Users\{windows-username}> python -m pip install numpy
+Collecting numpy
+  Obtaining dependency information for numpy from https://files.pythonhosted.org/packages/57/09/fe9282ffb0217176b0185900945189b6beaec4f94ff46afb76bcd9b68e30/numpy-1.26.1-cp310-cp310-win_amd64.whl.metadata
+  Downloading numpy-1.26.1-cp310-cp310-win_amd64.whl.metadata (61 kB)
+     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 61.2/61.2 kB 1.6 MB/s eta 0:00:00
+Downloading numpy-1.26.1-cp310-cp310-win_amd64.whl (15.8 MB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 15.8/15.8 MB 24.2 MB/s eta 0:00:00
+Installing collected packages: numpy
+Successfully installed numpy-1.26.1
+
+[notice] A new release of pip is available: 23.2.1 -> 23.3.1
+[notice] To update, run: python.exe -m pip install --upgrade pip
+PS C:\Users\{windows-username}>
+
+
+
+ """
