1e5ed1ad82e11fbaf4e8a965d9b556f0bf9407e8
spenright <stephen.p.enright@gmail.com>
2023-11-02 21:09:21 -0700

    Create snip-Twitter-20231101-1903.py
    Added snipe from Twitter for 7:03 PM · Nov 1, 2023

---
+++ snip-Twitter-20231101-1903.py
@@ -0,0 +1,12 @@
+# 7:03 PM · Nov 1, 2023
+#
+# https://twitter.com/clcoding/status/1719898071683706962
+#
+print( []*3 )
+#
+# Output:
+# A. [], [], []
+# B. [[], [], []]
+# C. []
+# D. ValueError
+#
